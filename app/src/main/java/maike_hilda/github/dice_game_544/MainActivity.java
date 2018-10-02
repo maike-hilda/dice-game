39d05bc39bc76d3773f39ae35e2b7d6f99700d39
Derek <derekjohnbaker23@gmail.com>
2018-10-01 21:32:08 -0500

    application shell built

---
+++ app/src/main/java/maike_hilda/github/dice_game_544/MainActivity.java
@@ -0,0 +1,13 @@
+package maike_hilda.github.dice_game_544;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
