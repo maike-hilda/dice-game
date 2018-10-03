8797ae6b1089e9236d3d987a2a62ba440747480f
Derek <derekjohnbaker23@gmail.com>
2018-10-03 01:01:52 -0500

    Input and input checking works

---
+++ app/src/main/java/maike_hilda/github/dice_game_544/DisplayMessageActivity.java
@@ -0,0 +1,13 @@
+package maike_hilda.github.dice_game_544;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
